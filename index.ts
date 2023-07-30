468dbd7cae9cb3d3347615c50afd4c1acd133192
Avi Mamenko <avimamenko@gmail.com>
2023-07-30 02:20:20 -0400

    Added mongoose connection string and db models

--- index.ts
+++ index.ts
@@ -1,13 +1,31 @@
 import { Context, APIGatewayProxyCallback } from "aws-lambda";
+import mongoose from "mongoose";
+import { User } from "./models/User";
+
+let conn = null;
+
+const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CLUSTER}.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`;
 
 export const handler = async (
   event: any,
   context: Context,
   callback: APIGatewayProxyCallback
 ) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Testing Lambda GitHub actions TypeScript!"),
-  };
-  return response;
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  if (conn == null) {
+    conn = mongoose.createConnection(uri, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    await conn.asPromise();
+    // conn.model("Test", new mongoose.Schema({ name: String }));
+  }
+
+  // const M = conn.model("Test");
+
+  const doc = await User.findOne();
+  console.log(doc);
+
+  return doc;
 };
