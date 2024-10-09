07135cd670c1665259c5c49055f00327fb924e1e
Avi Mamenko <avimamenko@gmail.com>
2024-10-09 15:17:31 -0400

    Updated env var names of aws access key ID and secret access key

--- lambdas/tikTokPuppeteerFunction/functions/writeScreenshotToS3.ts
+++ lambdas/tikTokPuppeteerFunction/functions/writeScreenshotToS3.ts
@@ -24,8 +24,8 @@ export const writeScreenshotToS3 = async ({
   const client = new S3Client({
     region: "us-east-1",
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
     },
   });
 
