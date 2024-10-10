676bb40194cd44ceeeeefd010121fbd714c372e6
Avi Mamenko <avimamenko@gmail.com>
2024-10-10 00:16:39 -0400

    Updated xvfb start script and Dockerfile

--- lambdas/tikTokPuppeteerFunction/start.sh
+++ lambdas/tikTokPuppeteerFunction/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Remove the Xvfb lock file if it exists
+if [ -e /tmp/.X99-lock ]; then
+    echo "Removing Xvfb lock file..."
+    rm -f /tmp/.X99-lock
+fi
+
 # Start Xvfb in the background
 Xvfb :99 -screen 0 1280x1024x24 &
 
