67f4fafe2d653f0ef09383821258245caf236a3b
Avi Mamenko <avimamenko@gmail.com>
2024-10-10 14:00:10 -0400

    Updated Dockerfile with docs from puppeteer-real-browser

--- lambdas/tikTokPuppeteerFunction/start.sh
+++ lambdas/tikTokPuppeteerFunction/start.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Remount /tmp/.X11-unix as read-write
-mount -o remount,rw /tmp/.X11-unix
-
 # Remove the Xvfb lock file if it exists
 if [ -e /tmp/.X99-lock ]; then
     echo "Removing Xvfb lock file..."
     rm -f /tmp/.X99-lock
 fi
-
-echo "Starting Xvfb with the following directory contents:"
-ls -al
 
 # Start Xvfb without listening on IPv6
 Xvfb :99 -screen 0 1280x1024x24 -nolisten tcp &
