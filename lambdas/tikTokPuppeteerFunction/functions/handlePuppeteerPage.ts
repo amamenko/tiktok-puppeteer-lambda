5721695ee7e13f53b6d29351e45863a3b1b4d18f
Avi Mamenko <avimamenko@gmail.com>
2024-10-13 00:31:42 -0400

    Commenting out tiktok-cookie-banner display none logic

--- lambdas/tikTokPuppeteerFunction/functions/handlePuppeteerPage.ts
+++ lambdas/tikTokPuppeteerFunction/functions/handlePuppeteerPage.ts
@@ -69,17 +69,17 @@ export const handlePuppeteerPage = async (browser: Browser) => {
 
     await waitForTimeout(2000);
 
-    try {
-      // Make sure the cookie banner is hidden so further click events register
-      await page.evaluate(() => {
-        const cookieBanner = document.querySelector(
-          "tiktok-cookie-banner"
-        ) as HTMLElement;
-        if (cookieBanner) cookieBanner.style.display = "none";
-      });
-    } catch (e) {
-      console.error(e);
-    }
+    // try {
+    //   // Make sure the cookie banner is hidden so further click events register
+    //   await page.evaluate(() => {
+    //     const cookieBanner = document.querySelector(
+    //       "tiktok-cookie-banner"
+    //     ) as HTMLElement;
+    //     if (cookieBanner) cookieBanner.style.display = "none";
+    //   });
+    // } catch (e) {
+    //   console.error(e);
+    // }
 
     if (isLocal || isDebug)
       await writeScreenshotToS3({
